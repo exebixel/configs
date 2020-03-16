732b5d5f0b50acbfda092b94421e19043cdcd399
exebixel <ezequielnat7@gmail.com>
2020-03-16 18:25:20 -0300

    fonts install add in install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+#add repo archlinuxcn 
 echo "[archlinuxcn]\nServer = http://repo.archlinuxcn.org/$arch" | sudo tee /etc/pacman.conf
 
+#packages will be installer
+#official and archlinxcn repos
 PACKS="i3-gaps polybar xfce4-notifyd \
-	flameshot playerctl rofi feh scrot"
+	flameshot playerctl rofi feh scrot \
+    hub xorg-xfd"
+#AUR
 YAYPACKS="siji-git"
 
 sudo pacman -Syu $PACKS --noconfirm
 yay -S $YAYPACKS --noconfirm
 
+#configs move
 cp -rv i3 $HOME/.config/
 cp -rv polybar $HOME/.config/
 cp -rv rofi $HOME/.config
+
+#Fonts install
+mkdir -p $HOME/.local/share/fonts
+cp -rv polybar/fonts/* $HOME/.local/share/fonts
+
+hub clone stark/siji
+cd siji
+./install.sh -d ~/.local/share/fonts
